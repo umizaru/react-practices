67cb74f101121c3923a8a84d3067ee1895cf0916
umizaru <y.wakita1t@gmail.com>
2023-09-15 08:26:14 +0900

    データ保存処理を実装（エラーあり）

--- my-app/src/MemoApp.js
+++ my-app/src/MemoApp.js
@@ -1,14 +1,22 @@
 import React, { useState } from "react";
-import "./App.css";
 import { v4 as uuidv4 } from "uuid";
+import "./App.css";
 import MemoList from "./MemoList";
 import MemoCreateButton from "./MemoCreateButton";
 import MemoEditForm from "./MemoEditForm";
-import memos from "./memos.json";
 
 function MemoApp() {
-  const [allMemos, setAllMemos] = useState(memos);
-  const [editingMemo, setEditingMemo] = useState(null);
+  const [allMemos, setAllMemos] = useState([]);
+  const [editingMemo, setEditingMemo] = useState("");
+
+  // 初期化したときにローカルストレージからデータを読み込む
+  function loadMemos() {
+    const storedMemos = JSON.parse(localStorage.getItem("memos")) || [];
+    setAllMemos(storedMemos);
+  }
+
+  // ページを読み込んだときにローカルストレージからデータを読み込む
+  window.addEventListener("load", loadMemos);
 
   function handleCreateButtonClick() {
     setEditingMemo({ id: uuidv4(), content: "新規メモ" });
@@ -24,11 +32,14 @@ function MemoApp() {
     const targetMemoIndex = allMemos.findIndex((memo) => memo.id === targetID);
 
     if (targetMemoIndex === -1) {
+      // 新規メモの場合
       setAllMemos((memos) => [
         ...memos,
         { id: uuidv4(), content: editingMemo.content },
       ]);
+      localStorage.setItem("memos", JSON.stringify(editingMemo));
     } else {
+      //既存メモの場合
       const newMemos = [...allMemos];
       const updateMemos = {
         ...newMemos[targetMemoIndex],
@@ -36,6 +47,7 @@ function MemoApp() {
       };
       newMemos[targetMemoIndex] = updateMemos;
       setAllMemos(newMemos);
+      localStorage.setItem("memos", JSON.stringify(newMemos));
     }
     setEditingMemo(null);
   }
@@ -48,6 +60,8 @@ function MemoApp() {
       return null;
     } else {
       setAllMemos(allMemos.filter((memo) => memo.id !== targetID));
+      localStorage.setItem("memos", JSON.stringify(editingMemo));
+      setEditingMemo(null);
     }
   }
 
